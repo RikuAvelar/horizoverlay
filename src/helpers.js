11d978ada8001b94008100e5a1f3954394d7033d
Riku Avelar <riku.avelar@gmail.com>
2021-06-10 19:28:00 -0400

    Fix lint error

--- src/helpers.js
+++ src/helpers.js
@@ -113,7 +113,7 @@ export function withHelper({
         this.props.config.configWindow.width
       },height=${this.props.config.configWindow.height}`
       this.configWindow = window.open(
-        `${location.pathname}/#/config`,
+        `${window.location.pathname}/#/config`,
         'Horizoverlay Config',
         windowFeatures
       )
